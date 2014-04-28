e12a45a2ba13e64ed843465fd0e313de06632efd
Serafinaa <estelle-73-@hotmail.fr>
2014-04-28 15:24:13 +0200

    Création de la class Music

---
+++ Music.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Music
+{
+
+    private int Id;
+    private string Nom_mp3;
+    private string Nom_picture;
+    private Music* next;
+
+	public Music()
+	{
+        this.Id = 0;
+        this.Nom_mp3 = "";
+        this.Nom_picture = "";
+        this.next = 0;
+	}
+}
