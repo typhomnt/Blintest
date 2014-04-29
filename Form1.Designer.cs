84d69059daa2275268b169339c652aa3f91b2499
alicedu38 <petitkaporal@hotmail.fr>
2014-04-29 16:56:42 +0200

    m

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace $safeprojectname$
+{
+    partial class Accueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Accueil));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxEquipe1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxEquipe2 = new System.Windows.Forms.TextBox();
+            this.TextBoxEquipe3 = new System.Windows.Forms.TextBox();
+            this.TextBoxEquipe4 = new System.Windows.Forms.TextBox();
+            this.TextBoxEquipe5 = new System.Windows.Forms.TextBox();
+            this.TextBoxEquipe6 = new System.Windows.Forms.TextBox();
+            this.TextBoxEquipe7 = new System.Windows.Forms.TextBox();
+            this.TextBoxEquipe8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnPlus = new System.Windows.Forms.Button();
+            this.BtnMoins = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(300, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Blindtest";
+            // 
+            // TextBoxEquipe1
+            // 
+            this.TextBoxEquipe1.Location = new System.Drawing.Point(50, 146);
+            this.TextBoxEquipe1.Name = "TextBoxEquipe1";
+            this.TextBoxEquipe1.Size = new System.Drawing.Size(108, 20);
+            this.TextBoxEquipe1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Les équipes :";
+            // 
+            // TextBoxEquipe2
+            // 
+            this.TextBoxEquipe2.Location = new System.Drawing.Point(50, 203);
+            this.TextBoxEquipe2.Name = "TextBoxEquipe2";
+            this.TextBoxEquipe2.Size = new System.Drawing.Size(108, 20);
+            this.TextBoxEquipe2.TabIndex = 3;
+            // 
+            // TextBoxEquipe3
+            // 
+            this.TextBoxEquipe3.Location = new System.Drawing.Point(50, 258);
+            this.TextBoxEquipe3.Name = "TextBoxEquipe3";
+            this.TextBoxEquipe3.Size = new System.Drawing.Size(108, 20);
+            this.TextBoxEquipe3.TabIndex = 4;
+            this.TextBoxEquipe3.Visible = false;
+            // 
+            // TextBoxEquipe4
+            // 
+            this.TextBoxEquipe4.Location = new System.Drawing.Point(50, 313);
+            this.TextBoxEquipe4.Name = "TextBoxEquipe4";
+            this.TextBoxEquipe4.Size = new System.Drawing.Size(108, 20);
+            this.TextBoxEquipe4.TabIndex = 5;
+            this.TextBoxEquipe4.Visible = false;
+            // 
+            // TextBoxEquipe5
+            // 
+            this.TextBoxEquipe5.Location = new System.Drawing.Point(282, 146);
+            this.TextBoxEquipe5.Name = "TextBoxEquipe5";
+            this.TextBoxEquipe5.Size = new System.Drawing.Size(108, 20);
+            this.TextBoxEquipe5.TabIndex = 6;
+            this.TextBoxEquipe5.Visible = false;
+            // 
+            // TextBoxEquipe6
+            // 
+            this.TextBoxEquipe6.Location = new System.Drawing.Point(282, 203);
+            this.TextBoxEquipe6.Name = "TextBoxEquipe6";
+            this.TextBoxEquipe6.Size = new System.Drawing.Size(108, 20);
+            this.TextBoxEquipe6.TabIndex = 7;
+            this.TextBoxEquipe6.Visible = false;
+            // 
+            // TextBoxEquipe7
+            // 
+            this.TextBoxEquipe7.Location = new System.Drawing.Point(282, 258);
+            this.TextBoxEquipe7.Name = "TextBoxEquipe7";
+            this.TextBoxEquipe7.Size = new System.Drawing.Size(108, 20);
+            this.TextBoxEquipe7.TabIndex = 8;
+            this.TextBoxEquipe7.Visible = false;
+            // 
+            // TextBoxEquipe8
+            // 
+            this.TextBoxEquipe8.Location = new System.Drawing.Point(282, 313);
+            this.TextBoxEquipe8.Name = "TextBoxEquipe8";
+            this.TextBoxEquipe8.Size = new System.Drawing.Size(108, 20);
+            this.TextBoxEquipe8.TabIndex = 9;
+            this.TextBoxEquipe8.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(450, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 46);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "JOUER !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnPlus
+            // 
+            this.BtnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlus.Location = new System.Drawing.Point(180, 103);
+            this.BtnPlus.Name = "BtnPlus";
+            this.BtnPlus.Size = new System.Drawing.Size(25, 33);
+            this.BtnPlus.TabIndex = 11;
+            this.BtnPlus.Text = "+";
+            this.BtnPlus.UseVisualStyleBackColor = true;
+            this.BtnPlus.Click += new System.EventHandler(this.BtnPlus_Click);
+            // 
+            // BtnMoins
+            // 
+            this.BtnMoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMoins.Location = new System.Drawing.Point(244, 103);
+            this.BtnMoins.Name = "BtnMoins";
+            this.BtnMoins.Size = new System.Drawing.Size(25, 33);
+            this.BtnMoins.TabIndex = 12;
+            this.BtnMoins.Text = "-";
+            this.BtnMoins.UseVisualStyleBackColor = true;
+            this.BtnMoins.Click += new System.EventHandler(this.BtnMoins_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 99);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Accueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(571, 350);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnMoins);
+            this.Controls.Add(this.BtnPlus);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBoxEquipe8);
+            this.Controls.Add(this.TextBoxEquipe7);
+            this.Controls.Add(this.TextBoxEquipe6);
+            this.Controls.Add(this.TextBoxEquipe5);
+            this.Controls.Add(this.TextBoxEquipe4);
+            this.Controls.Add(this.TextBoxEquipe3);
+            this.Controls.Add(this.TextBoxEquipe2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBoxEquipe1);
+            this.Controls.Add(this.label1);
+            this.Name = "Accueil";
+            this.Text = "Blindtest";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxEquipe1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBoxEquipe2;
+        private System.Windows.Forms.TextBox TextBoxEquipe3;
+        private System.Windows.Forms.TextBox TextBoxEquipe4;
+        private System.Windows.Forms.TextBox TextBoxEquipe5;
+        private System.Windows.Forms.TextBox TextBoxEquipe6;
+        private System.Windows.Forms.TextBox TextBoxEquipe7;
+        private System.Windows.Forms.TextBox TextBoxEquipe8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnPlus;
+        private System.Windows.Forms.Button BtnMoins;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
